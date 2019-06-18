ccadd1d497a2f1f46052e883d1ac2994a8f607ba
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-06-18 04:05:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,15 +3,12 @@ def unsafe?(speed)
 		puts "unsafe"
 	elsif "#{speed}" <= 40
 		puts "unsafe"
-	else "#{speed}" == 40 - 60 
+	else "#{speed}" == 40 - 60
 		puts "unsafe"
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
-	
-
-
